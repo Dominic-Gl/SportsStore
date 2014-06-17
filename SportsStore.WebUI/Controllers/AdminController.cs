2e082580c39c8608d8c6c38d351bc381ac9d7037
Gluba <d.gluba@googlemail.com>
2014-06-17 23:09:01 +0200

    Admin app began

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        
+        public ViewResult Index()
+        {
+
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productid)
+        {
+            Product product = repository.Products.FirstOrDefault(item => item.ProductID == productid);
+
+            return View(product);
+        }
+    }
+}
