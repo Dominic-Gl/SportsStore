28c9f06d61dba741df4a01feb46bdc614459aa42
Gluba <d.gluba@googlemail.com>
2014-07-01 23:12:50 +0200

    controllers and actions startedd

---
+++ ControllersAndActions/Controllers/BasicController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllersAndActions.Controllers
+{
+    public class BasicController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string controller = requestContext.RouteData.Values["controller"] as string;
+            string action = requestContext.RouteData.Values["action"] as string;
+
+            requestContext.HttpContext.Response.Write("Controller: " + controller +   ", Action: " +  action);
+        }
+    }
+}
---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using ControllersAndActions.Infrastructure;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            ViewBag.Message = " Hello from the derived controller index method";
+            
+            return View("MyView");
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            
+                return new RedirectResult("/Basic/index");
+            
+        }
+    }
+}
